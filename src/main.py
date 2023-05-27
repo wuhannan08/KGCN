060b1b461a846baff5241ed8d0239d8cb4599a9d
wuhannan <1242865419@qq.com>
2023-05-27 11:51:28 +0800

    增加评估指标acc、precision、recall

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ np.random.seed(555)
 parser = argparse.ArgumentParser()
 
 # movie
-parser.add_argument('--dataset', type=str, default='movie', help='which dataset to use')
+parser.add_argument('--dataset', type=str, default='github', help='which dataset to use')
 parser.add_argument('--aggregator', type=str, default='sum', help='which aggregator to use')
 parser.add_argument('--n_epochs', type=int, default=10, help='the number of epochs')
 parser.add_argument('--neighbor_sample_size', type=int, default=4, help='the number of neighbors to be sampled')
